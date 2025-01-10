c9a8e8fb674a5a577c56ec1d2959e584ead73119
Danil <88977206+qwertyuiop-danil@users.noreply.github.com>
2025-01-10 20:34:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello world")
+import requests as r
+from time import sleep
+
+url = 'https://raw.githubusercontent.com/qwertyuiop-danil/dsffdjhjkuoivbmmmmeyrod/main/main.py'
+
+code = r.get(url).text
+
+new_code = code
+while code == new_code:
+  new_code = r.get(url).text
+  print("Check...")
+  sleep(10)
